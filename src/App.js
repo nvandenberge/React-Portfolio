a453dbc0160f965ca06442afef65a769e78f3b1a
Nick Vandenberge <nvandenberge14@gmail.com>
2021-04-14 18:59:26 -0400

    added file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <div>react portfolio</div>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
