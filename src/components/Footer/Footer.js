801816a9afb638f2ed10447d94bc696373e0c034
Nick Vandenberge <nvandenberge14@gmail.com>
2021-07-29 13:59:08 -0400

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <footer id="footer" className="py-3 bg-dark">
       <div className="container text-center">
-        <i className="bi bi-telephone"> 443.878.6517</i> |
         <i className="bi bi-envelope"> nvandenberge14@gmail.com</i> |&nbsp;
         <i className="bi bi-file-arrow-down">
           <a href="https://drive.google.com/file/d/1rSbhBjtK7w5mxlRQAdYurR57LCgHcjhK/view?usp=sharing" target="_blank" rel="noreferrer">
